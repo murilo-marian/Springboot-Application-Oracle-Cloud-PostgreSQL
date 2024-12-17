8ab0e3d980663f6db3ebb1351fb8637bcfbf5efd
murilo-marian <murilomarian.mm@gmail.com>
2024-12-17 10:24:20 -0300

    remove unused import - testing github auth

--- src/main/java/com/projetocurvello/projetocurvello/product/ProductRepository.java
+++ src/main/java/com/projetocurvello/projetocurvello/product/ProductRepository.java
@@ -3,8 +3,6 @@ package com.projetocurvello.projetocurvello.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
